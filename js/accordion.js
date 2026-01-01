348be0076746e4c78f7eb4edc96419939215242f
Jeya-Jothi <jeyajothi108@gmail.com>
2026-01-01 13:54:34 +0530

    Add Collection section with responsive

---
+++ js/accordion.js
@@ -0,0 +1,18 @@
+const accordionItems = document.querySelectorAll(".accordion-item");
+
+accordionItems.forEach((item) => {
+  const header = item.querySelector(".accordion-header");
+
+  header.addEventListener("click", () => {
+    accordionItems.forEach((el) => {
+      if (el !== item) {
+        el.classList.remove("active");
+        el.querySelector(".icon").textContent = "+";
+      }
+    });
+
+    item.classList.toggle("active");
+    const icon = item.querySelector(".icon");
+    icon.textContent = item.classList.contains("active") ? "−" : "+";
+  });
+});
